df027cb2792b34c502abe6587fa1c50810f583a9
furkan <furkangray@gmail.com>
2024-03-09 19:30:31 +0300

    Initial Commit

---
+++ todo-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  todo-app
+//
+//  Created by furkan gurcay on 9.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ todo-app/todo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_appApp.swift
+//  todo-app
+//
+//  Created by furkan gurcay on 9.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct todo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
